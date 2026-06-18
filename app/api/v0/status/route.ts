33cdab9b2f9f122c4ecbb7821c6214934516a8bf
Ramon Guelber Martins <rgmguelber@gmail.com>
2026-06-17 22:21:48 -0300

    test test routines and implement status api

---
+++ app/api/v0/status/route.ts
@@ -0,0 +1,5 @@
+export async function GET(request: Request, response: Response) {
+  return Response.json({ mensagem: "Sucesso" });
+}
+
+// TODO: Aula Dia 16 - Versionamento de API e Endpoint "/status"
