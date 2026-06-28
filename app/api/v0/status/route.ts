5b8784b9965424098ddbb8f240c2169e3eccfc0b
Ramon Guelber Martins <rgmguelber@gmail.com>
2026-06-28 00:55:04 -0300

    add `database.ts` and `.env` files

--- app/api/v0/status/route.ts
+++ app/api/v0/status/route.ts
@@ -1,4 +1,8 @@
+import database from "@/infra/database";
+
 export async function GET(request: Request, response: Response) {
+  const result = await database.query("SELECT 1+1;");
+
   return Response.json({ mensagem: "Sucesso" });
 }
 
