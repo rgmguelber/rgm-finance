5419b74f20267afb9505c5d73e42f1a50935e212
Ramon Guelber Martins <rgmguelber@gmail.com>
2026-06-29 18:52:11 -0300

    finish `/api/v0/status` endpoint

--- app/api/v0/status/route.ts
+++ app/api/v0/status/route.ts
@@ -1,9 +1,36 @@
 import database from "@/infra/database";
 
 export async function GET(request: Request, response: Response) {
-  const result = await database.query("SELECT 1+1;");
+  // Data da atualização
+  const updateAt = new Date().toISOString();
 
-  return Response.json({ mensagem: "Sucesso" });
+  // Versão do banco de dados
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+
+  // Máximo de conexões
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnections =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  // Conexões ativas
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(datid) as opened_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+  const databaseOpenedConnections =
+    databaseOpenedConnectionsResult.rows[0].opened_connections;
+
+  return Response.json({
+    update_at: updateAt,
+    database: {
+      server_version: databaseVersion,
+      max_connections: parseInt(databaseMaxConnections),
+      opened_connections: parseInt(databaseOpenedConnections),
+    },
+  });
 }
 
-// TODO: Aula Dia 19 - Foi certo fazer o commit do .env?
+// TODO: Aula Dia 20 - Endpoint "/status": ISO 8601 + Fuso
--- tests/integration/api/v0/status/get.test.js
+++ tests/integration/api/v0/status/get.test.js
@@ -1,5 +1,24 @@
 test("GET to api/v0/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v0/status");
 
+  // Verifica sucesso da requisição
   expect(response.status).toBe(200);
+
+  // Verifica sucesso do retorno do update_at
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  const parsedDate = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedDate);
+
+  // Verifica sucesso do retorno da versão do banco de dados
+  expect(responseBody.database.server_version).toMatch(/^(\d+\.\d+)/, "16.14");
+
+  // verifica sucesso do retorno da quantidade máxima de conexões
+  expect(responseBody.database.max_connections).toEqual(100);
+
+  // Verifica sucesso do retorno do número de conexões abertas
+  expect(responseBody.database.opened_connections).toEqual(1);
+
+  console.log(responseBody);
 });
